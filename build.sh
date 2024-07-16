2ed702ef0aad10cfb77bb49624ea77e9eda5cd53
rorudzhov <orudzhov.ra@gmail.com>
2024-07-16 23:18:22 +0300

    Update build.sh | Add archive step

--- build.sh
+++ build.sh
@@ -1,7 +1,11 @@
 GOOS=linux # REQUIRED REPLACE
-GOARCH=amd64 # REQUIRED REPLACE
+GOARCH=arm64 # REQUIRED REPLACE
 
 export GOARCH=$GOARCH
 export GOOS=$GOOS
-file="ds389-exporter"
+file="ds389-exporter-$GOOS-$GOARCH"
 go build -ldflags "-s -w" -o $file ds389-exporter.go
+
+tar czvf $file.tar.gz $file
+rm $file
+
